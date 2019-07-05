75644686f58b89d76f6f4ca6a99971619d09331b
Kendrick Ang <phantom2457@hotmail.com>
2019-07-05 21:34:55 +0800

    Checkpoint #1: Rearranged files, base template

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/App.css';
+
+class App extends React.Component{
+	render() {
+		return (
+			<div className="App">
+				<p>Insert components here</p>
+			</div>
+		);
+	}
+}
+
+
+export default App;
