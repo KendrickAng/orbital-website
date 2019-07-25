6e4f3288f2a84a0dd17591477dc899e67fce0947
WeomuCat <45603243+WeomuCat@users.noreply.github.com>
2019-07-26 03:18:24 +0800

    Update index.js
    Make Getting Started clearer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,7 @@ import { scoresReducer } from './scoresReducer';
 
 //Reducers specify how the application's state changes in response to actions sent to the store.
 
-const gettingStartedContent = ["1. Download the .zip file from the homepage", "2. Unzip & extract the .exe game file", "3. Run the game. Enjoy :)"];
+const gettingStartedContent = ["1. Download the .zip file from the homepage", "2. Unzip the game files", "3. Run the game executable. Enjoy :)"];
 const tankContent = ["Primary skill: Block", "Secondary skill: Hammer swing", "Tertiary skill: Fortress"];
 const assassinContent = ["Primary skill: Dash", "Secondary skill: Shuriken throw", "Tertiary skill: Cleanse"];
 
